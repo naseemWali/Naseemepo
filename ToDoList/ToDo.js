00d30ad909764479b5d9544007c735d8cb1b9beb
naseemwali <naseemwalikhan776@gmail.com>
2025-07-23 23:53:48 +0500

    addedonjuly

--- ToDoList/ToDo.js
+++ ToDoList/ToDo.js
@@ -18,17 +18,29 @@ function add() {
         span.innerHTML='\u00d7';
         li.appendChild(span)
     }
-    input.value=''
+    input.value='';
+    savedate();
 }
 
 listcontainer.addEventListener('click',function (e) {
 
     if (e.target.tagName==='LI') {
         e.target.classList.toggle('checked');
+        savedate()
     }
     else if (e.target.tagName==='SPAN') {
         e.target.parentElement.remove()
+        savedate()
     }
 },false)
 
 
+
+function savedate() {
+    localStorage.setItem('data',listcontainer.innerHTML)
+}
+
+function showdata() {
+    listcontainer.innerHTML=localStorage.getItem('data')
+}
+showdata()
